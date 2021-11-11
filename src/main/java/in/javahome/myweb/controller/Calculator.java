cbd7eda213393a9838f590003d81aba4a6ae2163
pns99 <91236752+pns99@users.noreply.github.com>
2021-11-11 18:38:32 +0800

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -21,6 +21,11 @@ public class Calculator {
 
 ###############################*/
 
+package in.javahome.myweb.controller;
+import java.util.List;
+/*
+ * 
+ */
 public class Calculator {
 	/*
 	 * @param i
@@ -35,20 +40,11 @@ public class Calculator {
 	}
 }
 
-public class Calculator {
-	/*
-	 * @param i
-	 * @param j
-	 * @return int
-	 */
-	public int add(int i, int j){
-		return i+j;
-	}
-	public int multiply(int i, int j){
-		return i*j;
-	}
-}
-
+package in.javahome.myweb.controller;
+import java.util.List;
+/*
+ * 
+ */
 public class Calculator {
 	/*
 	 * @param i
