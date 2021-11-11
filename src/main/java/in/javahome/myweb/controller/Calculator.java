cfa70a477f95623de30e1c72b728a397565d35af
pns99 <91236752+pns99@users.noreply.github.com>
2021-11-11 18:34:01 +0800

    testing quality checks fails

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -16,3 +16,49 @@ public class Calculator {
 		return i*j;
 	}
 }
+
+/*#################################
+
+###############################*/
+
+public class Calculator {
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+}
+
+public class Calculator {
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+}
+
+public class Calculator {
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+}
