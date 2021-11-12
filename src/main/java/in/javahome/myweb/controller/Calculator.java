0a606be49b3067e117c623e96af72813f7c1518a
pns99 <91236752+pns99@users.noreply.github.com>
2021-11-12 11:56:34 +0800

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -16,39 +16,3 @@ public class Calculator {
 		return i*j;
 	}
 }
-/**********************************************************************/
-
-
-
-class Main {
-
-  public static void main(String[] args) {
-    
-    System.out.println("Enter two numbers");
-    int first = 10;
-    int second = 20;
-    
-    System.out.println(first + " " + second);
-
-    // add two numbers
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
-  }
-}
-
-
-class Main {
-
-  public static void main(String[] args) {
-    
-    System.out.println("Enter two numbers");
-    int first = 10;
-    int second = 20;
-    
-    System.out.println(first + " " + second);
-
-    // add two numbers
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
-  }
-}
