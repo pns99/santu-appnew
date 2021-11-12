217c916a89dbb220f4624de769908df6087674f2
pns99 <91236752+pns99@users.noreply.github.com>
2021-11-12 11:50:53 +0800

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -16,23 +16,13 @@ public class Calculator {
 		return i*j;
 	}
 }
-/************************************************************************************************
-package in.javahome.myweb.controller;
-import java.util.List;
-/*
- * 
- */
-public class Calculator {
-	/*
-	 * @param i
-	 * @param j
-	 * @return int
-	 */
-	public int add(int i, int j){
-		return i+j;
-	}
-	public int multiply(int i, int j){
-		return i*j;
-	}
-}
-
+/**********************************************************************/
+AtomicInteger aInt1 = new AtomicInteger(0);
+AtomicInteger aInt2 = new AtomicInteger(0);
+if (aInt1.equals(aInt2)) { ... } 
+if (aInt1.equals(aInt2)) { ... } 
+if (aInt1.equals(aInt2)) { ... } 
+if (aInt1.equals(aInt2)) { ... } 
+if (aInt1.equals(aInt2)) { ... } 
+if (aInt1.equals(aInt2)) { ... } 
+if (aInt1.equals(aInt2)) { ... } 
