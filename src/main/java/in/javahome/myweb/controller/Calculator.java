de2a8f4b337713856eed0916164280788f053087
pns99 <91236752+pns99@users.noreply.github.com>
2021-11-12 11:52:52 +0800

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -17,12 +17,38 @@ public class Calculator {
 	}
 }
 /**********************************************************************/
-AtomicInteger aInt1 = new AtomicInteger(0);
-AtomicInteger aInt2 = new AtomicInteger(0);
-if (aInt1.equals(aInt2)) { ... } 
-if (aInt1.equals(aInt2)) { ... } 
-if (aInt1.equals(aInt2)) { ... } 
-if (aInt1.equals(aInt2)) { ... } 
-if (aInt1.equals(aInt2)) { ... } 
-if (aInt1.equals(aInt2)) { ... } 
-if (aInt1.equals(aInt2)) { ... } 
+
+
+
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 10;
+    int second = 20;
+    
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
+}
+
+
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 10;
+    int second = 20;
+    
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
+}
